176b39813572f1ba38adb2012bd6130d6993353c
Neha Jain <nehajain0311@gmail.com>
2022-11-07 09:12:49 -0500

    add functionality added

--- todosite/todoapp/urls.py
+++ todosite/todoapp/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
-from .views import index,delete,add
+from .views import index,delete,add,update
 
 app_name="todoapp"
 
 urlpatterns = [
     path("",index,name="index"),
     path("add/",add,name="add"),
+    path("update/<int:id>/",update,name="update"),
     path("delete/<int:task_id>/",delete,name="delete"),
     
 ]
--- todosite/todoapp/views.py
+++ todosite/todoapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
+
+
 from .models import Task
+from .forms import TaskForm
+
 
 # Create your views here.
 def index(request):
@@ -7,8 +11,22 @@ def index(request):
     return render(request,'todoapp/index.html',{'tasks':task_list})
 
 def add(request):
-    
+    if request.method=='POST':
+        text=request.POST.get('text')
+        priority=request.POST.get('priority')
+        task=Task(text=text,priority=priority)
+        task.save()
+        return redirect("/")
     return render(request,'todoapp/add.html')
+
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form=TaskForm(request.POST or None,instance=task)
+    if request.method=="POST":
+        if form.is_valid():
+            form.save()        #form is associated with model, so it will save model too
+        return redirect("/")
+    return render(request,'todoapp/update.html',{'form':form,'task':task})
 
 def delete(request,task_id):
     task=Task.objects.get(id=task_id)
